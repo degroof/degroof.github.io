1a06f5b10f163f9045193847d18c82da2a0c627b
degroof <steve.degroof@gmail.com>
2025-09-11 15:06:44 -0400

    Add files via upload

--- tmwtd.js
+++ tmwtd.js
@@ -1141,3 +1141,9 @@
     var tasks = new Tasks();
     load();
     updateMainScreenState();
+    window.addEventListener("load",function() {
+        setTimeout(function(){
+            // This hides the address bar:
+            window.scrollTo(0, 1);
+        }, 0);
+    });
